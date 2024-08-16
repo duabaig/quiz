f55fcf73a29ad1451e8aea45cc6f9c60080dd33a
duabaig <duaabaig70@gmail.com>
2024-08-16 08:08:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+var Q1 = prompt("What attribute is used to define the source of an image in html?\n (a):href (b):scr (c):link")
+if(Q1==="(b)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q2 = prompt("Which of the following CSS properties is used to set the opacity of an element?\n (a):transparency (b):visibility (c):opacity")
+if(Q2==="(c)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q3 = prompt("Which of the following JavaScript methods is used to sort an array in descending order?\n (a):sort().reverse()(b):sort() (c):sort(function(a, b) { return b - a; })")
+if(Q3==="(c)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q4 = prompt("Which of the following CSS selectors is used to select all elements with the class header?\n (a):.header (b):#header (c):[header]")
+if(Q4==="(a)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q5 = prompt("Which of the following JavaScript methods is used to convert a string to uppercase?\n (a):charAt() (b):toLowerCase() (c):toUpperCase()")
+if(Q5==="(c)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q6 = prompt("Which of the following JavaScript statements is used to declare a variable that can be reassigned?\n (a):let (b):var (c):function")
+if(Q6==="(b)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q7 = prompt("Which of the following JavaScript methods is used to add an element to the end of an array\n (a):push() (b):shift() (c):pop()")
+if(Q7==="(a)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q8 = prompt("Which of the following JavaScript methods is used to concatenate two strings?\n (a):join() (b):split() (c):concat()")
+if(Q8==="(c)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q9 = prompt("Which of the following JavaScript methods is used to find the index of an element in an array?\n (a):indexOf() (b):lastIndexOf() (c):includes()")
+if(Q9==="(a)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
+var Q10 = prompt("Which of the following JavaScript statements is used to create a new promise?\n (a): new Promise()(b):Promise.resolve()(c):Promise.reject()")
+if(Q10==="(a)"){
+    alert("correct")
+}
+else{
+    alert("incorrect")
+}
